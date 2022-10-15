2f63a43b5d9190facd9971b7e01529ed7982e3d1
article-i <artii@foxmail.com>
2022-10-15 19:13:06 +0800

    up

--- docs/public/js/index.js
+++ docs/public/js/index.js
@@ -23,7 +23,7 @@ var Star = function (x,y,radius,size,color) {
 		const lastPos = {x:this.x, y:this.y}
 
 		if(this.temp > 1) {
-			this.alpha -= 0.001;
+			this.alpha -= 0.006;
 		} else {
 			this.temp += 0.01;
 			this.alpha += 0.01;
